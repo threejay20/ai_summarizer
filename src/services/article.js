2df8c829b6576025e87d3b43591acc9950af0a81
threejay20 <118399217+threejay20@users.noreply.github.com>
2023-09-16 18:11:57 -0400

    Completed application

--- src/services/article.js
+++ src/services/article.js
@@ -1,5 +1,4 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 
@@ -12,14 +11,15 @@ export const articleApi = createApi({
             headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
 
             return headers;
-
-        }
+        },
     }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-          query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`
-        })
-    })
-});
+            // encodeURIComponent() function encodes special characters that may be present in the parameter values
+            // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
+            query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+        }),
+    }),
+})
 
-export const { useLazyGetSummaryQuery }  = articleApi;
+export const { useLazyGetSummaryQuery } = articleApi
